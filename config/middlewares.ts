6acb547f5e1ddfe2417070e71663b3157cf4f907
ZohaBAW <zohabawahid@gmail.com>
2025-10-05 20:08:29 +0400

    Update middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,17 @@
-export default [
-  'strapi::logger',
+module.exports = [
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:5175', 'https://off-road-777f7.web.app'],
+      methods: ['GET','POST','PUT','PATCH','DELETE','HEAD','OPTIONS'],
+      headers: ['Content-Type','Authorization','Origin','Accept'],
+      credentials: true,
+    },
+  },
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
